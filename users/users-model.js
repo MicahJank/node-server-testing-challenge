ee2b47053e283d7177fd3acd873559bbd3dec866
MicahJank <MicahJank@users.noreply.github.com>
2019-12-14 11:53:43 -0600

    finished boilerplate for the users-model

--- users/users-model.js
+++ users/users-model.js
@@ -1,3 +1,25 @@
-// findAll
-// findBy
-// findById
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+    find,
+    findById,
+    addUser,
+    removeUser,
+}
+
+
+function find() {
+    return null;
+}
+
+function findById() {
+    return null;
+}
+
+function addUser() {
+    return null;
+}
+
+function removeUser() {
+    return null;
+}
