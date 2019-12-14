d92e155fe5d4cea731b3ee64245926f26fb0ea0c
MicahJank <MicahJank@users.noreply.github.com>
2019-12-14 11:27:52 -0600

    created the first migrations file

---
+++ data/migrations/20191214110818_create-users-table.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', tbl => {
+    tbl.increments(); // <- gives us the id
+    tbl.string('username', 255).notNullable().unique(); // <- users may have the same NAME - but a username should be unique to each person registering
+    tbl.string('password', 255).notNullable(); // <- users can have the same password
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
